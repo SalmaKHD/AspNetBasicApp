e73786ec5284dc0eeb83c9b44403133a57b72b1d
Salma Khodaei <khodaeisalma8@gmail.com>
2026-02-12 10:49:42 -0800

    add basic context methods + middleware code

---
+++ FirstWebApplication/CustomMiddleware.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstWebApplication.CustomMiddleware
+{
+    public class CustomMiddleware: IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Hello ");
+            await next(context);
+            await context.Response.WriteAsync("coming back");
+        }
+    }
+}
--- FirstWebApplication/Program.cs
+++ FirstWebApplication/Program.cs
@@ -1,6 +1,52 @@
+using FirstWebApplication;
+using FirstWebApplication.CustomMiddleware;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args); // creates a builder that confgures initial set up for a web app
 var app = builder.Build(); // creates an instance of a web app
+// add a custom middleware
+builder.Services.AddTransient<CustomMiddleware>();
 
-app.MapGet("/", () => "Hello World!"); // when getting a request to root, return "Hello World!"
+// create a middleware
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+
+    await context.Response.WriteAsync("Hello ");
+    await next(context); // pass to next middleware
+    await context.Response.WriteAsync("coming back");
+});
+
+// executed for every single request -> a way to create a middleware
+// Run() does not forward requests to any other middlewares
+app.Run(async (HttpContext context) =>
+{   // terminating middleware
+    // add a status code to response
+    context.Response.StatusCode = 400;
+    // add headers
+    context.Response.Headers["MyKey"] = "Salma";
+    // get info about the request
+    Util.printValue(context.Request.Path);
+    Util.printValue(context.Request.Method);
+
+    if(context.Request.Method == "GET")
+    {
+        if(context.Request.Query.ContainsKey("id"))
+        {
+            Util.printValue(context.Request.Query["id"]);
+        }
+    }
+
+    // check for requests headers
+    if(context.Request.Headers.ContainsKey("User-Agent"))
+    {
+        Util.printValue(context.Request.Headers["User-Agent"]);
+    }
+
+    // read body
+    StreamReader sr = new StreamReader(context.Request.Body);
+    Util.printValue(await sr.ReadToEndAsync());
+
+    await context.Response.WriteAsync("Hello");
+});
 
 app.Run();
