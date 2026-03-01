ecf5405517f05cea8dae6e9497d8d541fa925da7
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-01 01:34:07 -0800

    add and pass model to partial view

---
+++ FirstWebApplication/Models/Cities.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstWebApplication.Models
+{
+    public class Cities
+    {
+        public string Title { get; set; }
+
+        public Cities(string title)
+        {
+            Title = title;
+        }
+    }
+}
