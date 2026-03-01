918a875b92b80b2136d1f7c69e67d2d2e8d0e28a
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-01 02:16:34 -0800

    add a view component

---
+++ FirstWebApplication/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.ViewComponents
+{
+    // returns data for a component (partial view)
+    [ViewComponent]
+    public class GridViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // independent from ViewData of each layout
+            ViewData["title"] = "Grid View Component";
+            return View(); // -> fills data for a partial view
+        }
+    }
+}
