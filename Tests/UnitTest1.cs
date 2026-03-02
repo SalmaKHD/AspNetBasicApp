17777e50e1cdb13262bc4f3f2411b7f88ed4f78e
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-02 09:00:06 -0800

    add test example

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            MathTestClass mtc = new MathTestClass();
+            int a = 10, b = 20;
+            int expected = 30;
+
+            // Act
+            int result = mtc.Add(a, b);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
