8e3cdcb398eba585554633e62d984dcfa6ce83d5
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-07 15:31:11 -0800

    add endpoint filter

--- FirstWebApplication/MapGroups/CountriesMaGroup.cs
+++ FirstWebApplication/MapGroups/CountriesMaGroup.cs
@@ -1,4 +1,5 @@
 ﻿using Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 
@@ -24,7 +25,7 @@ namespace FirstWebApplication.MapGroups
             {
                 countries.Add(new Country(name: name));
 
-                await context.Response.WriteAsync("Add successful");
+                return Results.Ok("Add successful");
             });
 
             builder.MapGet("/{id:string}", async (HttpContext context, string id) =>
@@ -37,10 +38,177 @@ namespace FirstWebApplication.MapGroups
                 }
                 else
                 {
-                    await context.Response.WriteAsync("No country found");
+                    return Results.BadRequest(new
+                    {
+                        Message = "No countries found."
+                    });
                 }
 
             });
+
+            return builder;
+        }
+    }
+}
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FirstWebApplication.MapGroups
+{
+    public static class CountriesMaGroup
+    {
+        public static RouteGroupBuilder CountriesApi(this RouteGroupBuilder builder)
+        {
+            List<Country> countries = new List<Country> {
+            new Country("Brazil"),
+            new Country("Canada")
+            };
+
+            builder.MapGet("/", async (HttpContext context) =>
+            {
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize(countries));
+            });
+
+
+            builder.MapPost("/", async (HttpContext context, string name) =>
+            {
+                countries.Add(new Country(name: name));
+
+                return Results.Ok("Add successful");
+            });
+
+            builder.MapGet("/{id:string}", async (HttpContext context, string id) =>
+            {
+                Country country = countries.First(country => country.CountryID == Guid.Parse(id));
+                if (country != null)
+                {
+                    await context.Response.WriteAsync(
+                                JsonSerializer.Serialize(countries));
+                }
+                else
+                {
+                    return Results.BadRequest(new
+                    {
+                        Message = "No countries found."
+                    });
+                }
+
+            });
+
+            return builder;
+        }
+    }
+}
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FirstWebApplication.MapGroups
+{
+    public static class CountriesMaGroup
+    {
+        public static RouteGroupBuilder CountriesApi(this RouteGroupBuilder builder)
+        {
+            List<Country> countries = new List<Country> {
+            new Country("Brazil"),
+            new Country("Canada")
+            };
+
+            builder.MapGet("/", async (HttpContext context) =>
+            {
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize(countries));
+            });
+
+
+            builder.MapPost("/", async (HttpContext context, string name) =>
+            {
+                countries.Add(new Country(name: name));
+
+                return Results.Ok("Add successful");
+            });
+
+            builder.MapGet("/{id:string}", async (HttpContext context, string id) =>
+            {
+                Country country = countries.First(country => country.CountryID == Guid.Parse(id));
+                if (country != null)
+                {
+                    await context.Response.WriteAsync(
+                                JsonSerializer.Serialize(countries));
+                }
+                else
+                {
+                    return Results.BadRequest(new
+                    {
+                        Message = "No countries found."
+                    });
+                }
+
+            });
+
+            return builder;
+        }
+    }
+}
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FirstWebApplication.MapGroups
+{
+    public static class CountriesMaGroup
+    {
+        public static RouteGroupBuilder CountriesApi(this RouteGroupBuilder builder)
+        {
+            List<Country> countries = new List<Country> {
+            new Country("Brazil"),
+            new Country("Canada")
+            };
+
+            builder.MapGet("/", async (HttpContext context) =>
+            {
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize(countries));
+            });
+
+
+            builder.MapPost("/", async (HttpContext context, string name) =>
+            {
+                countries.Add(new Country(name: name));
+
+                // string format
+                return Results.Ok("Add successful"); // can be string or object
+            });
+
+            builder.MapGet("/{id:string}", async (HttpContext context, string id) =>
+            {
+                Country country = countries.First(country => country.CountryID == Guid.Parse(id));
+                if (country != null)
+                {
+                    return Results.Json(country);
+                }
+                else
+                {
+                    // json format
+                    return Results.BadRequest(new
+                    {
+                        Message = "No countries found."
+                    });
+                }
+
+            })
+                .AddEndpointFilter(async (EndpointFilterInvocationContext context, EndpointFilterDelegate next) =>
+                {
+                    string? id = context.Arguments.OfType<string>().First();
+                    if(id == null)
+                    {
+                        return Results.BadRequest("Id must be provided");
+                    }
+                    // execute subsequent filter or call action
+                    return await next(context);
+                });
 
             return builder;
         }
