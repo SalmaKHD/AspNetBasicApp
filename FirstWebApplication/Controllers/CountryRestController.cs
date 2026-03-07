17f40cc3c2c4498b4945b9d753ebfde35a3dee84
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-07 04:05:38 -0800

    add Cities method to Rest controller

--- FirstWebApplication/Controllers/CountryRestController.cs
+++ FirstWebApplication/Controllers/CountryRestController.cs
@@ -1,17 +1,50 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using ServiceContracts;
+using ServiceContracts.DTO;
 
 namespace FirstWebApplication.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [Route("api/country")]
-    [ApiController]
+    [ApiController] // allows options like automatic json request body parsing + 400 bad request when state not valid
     public class CountryRestController : ControllerBase
     {
-        [HttpGet]
-        public string Method()
+        private ICountriesService _countriesService;
+
+        public CountryRestController(ICountriesService countriesService)
         {
-            return "Hello from Web API";
+            _countriesService = countriesService;
+
+            // bad practice, ok until db implementation is ready
+            _countriesService.AddCountry(new CountryAddRequest
+            {
+                Name = "Brazil"
+            });
+
+            _countriesService.AddCountry(new CountryAddRequest
+            {
+                Name = "Canada"
+            });
+        }
+
+        [HttpGet] // we may imply this by method name, but not mandatory -> GetCountry
+        public ActionResult Cities()
+        {
+            // automatially done when adding attribute constraints
+            if(!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState); // for validation errors
+            }
+
+            var coutries = _countriesService.GetCountries();
+            if (coutries.IsNullOrEmpty())
+            {
+                // for reporting any kind of problems, can be used for any type of exceptions also
+                return Problem(detail: "No countries found", statusCode: 404, title: "Cities");
+            }
+            return Ok(coutries);
         }
     }
 }
