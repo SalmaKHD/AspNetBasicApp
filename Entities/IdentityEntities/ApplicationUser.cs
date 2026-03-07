0bc82289f32800b7ca306d21fced03260a4fa08a
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-07 00:56:03 -0800

    add identity user class

---
+++ Entities/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.IdentityEntities
+{
+    // continue when installing packages possible
+    //public class ApplicationUser: IdentityUser<Guid>
+    //{
+    //}
+}
