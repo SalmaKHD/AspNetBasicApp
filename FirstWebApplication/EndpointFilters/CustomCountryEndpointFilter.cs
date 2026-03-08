5e1c15f44753e1130ae5dfcd7382bd30ad0ec5e1
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-08 15:31:46 -0700

    add minimal api example

---
+++ FirstWebApplication/EndpointFilters/CustomCountryEndpointFilter.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FirstWebApplication.EndpointFilters
+{
+    public class CustomCountryEndpointFilter : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            string? id = context.Arguments.OfType<string>().First();
+            if (id == null)
+            {
+                return Results.BadRequest("Id must be provided");
+            }
+            // execute subsequent filter or call action
+            return await next(context);
+        }
+    }
+}
