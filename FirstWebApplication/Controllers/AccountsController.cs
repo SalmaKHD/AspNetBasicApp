b3960e700b38f37ad050f02aef8fde93e80ed152
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-11 17:34:43 -0700

    add register screen + buttons + controller

---
+++ FirstWebApplication/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts.DTO;
+
+namespace FirstWebApplication.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterDTO register)
+        {
+            //if (ModelState.IsValid)
+            //{
+
+            //}
+            //else
+            //{
+            //    ValidatorHel
+            //}
+            return View();
+        }
+    }
+}
---
+++ ServiceContracts/DTO/RegisterDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ServiceContracts.DTO
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage ="name cannot be empty")]
+        public string PersonName { get; set; }
+
+        [Required(ErrorMessage = "email cannot be empty")]
+        [EmailAddress(ErrorMessage ="email format incorrect")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "phone cannot be empty")]
+        [RegularExpression("^[0-9]*$", ErrorMessage ="Phone number format incorrect")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "password cannot be empty")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "confirm password cannot be empty")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
