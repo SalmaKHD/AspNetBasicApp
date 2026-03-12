a40349c71742512c60316b0db8e4c936b3d89199
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-12 08:44:34 -0700

    complete register controller

--- FirstWebApplication/Controllers/AccountsController.cs
+++ FirstWebApplication/Controllers/AccountsController.cs
@@ -1,4 +1,6 @@
 ﻿using Entities;
+using Entities.IdentityEntities;
+using FirstWebApplication.NewFolder4;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ServiceContracts.DTO;
@@ -8,9 +10,12 @@ namespace FirstWebApplication.Controllers
     [Route("[controller]/[action]")]
     public class AccountsController : Controller
     {
-        private readonly UserManager<ApplicationDbContext> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public 
+        public AccountsController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
         [HttpGet]
         public IActionResult Register()
@@ -19,17 +24,36 @@ namespace FirstWebApplication.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterDTO register)
+        public async Task<IActionResult> Register(RegisterDTO register)
         {
-            //if (ModelState.IsValid)
-            //{
-
-            //}
-            //else
-            //{
-            //    ValidatorHel
-            //}
-            return View();
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = register.Email,
+                    PhoneNumber = register.Phone,
+                    UserName = register.Email,
+                    PersonName = register.PersonName
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, register.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("Register", error.Description);
+                    }
+                    return View(register);
+                }
+            }
+            else
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(temp => temp.Errors).Select(temp => temp.ErrorMessage);
+                return View(register);
+            }
         }
     }
 }
