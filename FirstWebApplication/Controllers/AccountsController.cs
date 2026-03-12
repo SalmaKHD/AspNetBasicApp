c674b057343f1d2fa3355f0c949fee8650f49a43
Salma Khodaei <khodaeisalma8@gmail.com>
2026-03-12 10:21:41 -0700

    add login and logout screens + logic

--- FirstWebApplication/Controllers/AccountsController.cs
+++ FirstWebApplication/Controllers/AccountsController.cs
@@ -3,6 +3,7 @@ using Entities.IdentityEntities;
 using FirstWebApplication.NewFolder4;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
 using ServiceContracts.DTO;
 
 namespace FirstWebApplication.Controllers
@@ -58,6 +59,44 @@ namespace FirstWebApplication.Controllers
                 ViewBag.Errors = ModelState.Values.SelectMany(temp => temp.Errors).Select(temp => temp.ErrorMessage);
                 return View(register);
             }
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO login)
+        {
+            if (ModelState.IsValid)
+            {
+                // creates and gives back cookie automatically
+                var result = await _signInManager.PasswordSignInAsync(login.UserName, login.Password, isPersistent: true, lockoutOnFailure: true);
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("Login", "User does not exist");
+                    return View(login);
+                }
+                else
+                {
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+            }
+            else
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(temp => temp.Errors).Select(temp => temp.ErrorMessage);
+                return View(login);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            // deletes user cookie
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
---
+++ ServiceContracts/DTO/LoginDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ServiceContracts.DTO
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "email cannot be empty")]
+        [EmailAddress(ErrorMessage = "email format incorrect")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "password cannot be empty")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
